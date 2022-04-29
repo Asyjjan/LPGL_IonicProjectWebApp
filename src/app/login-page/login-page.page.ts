b17ad74b6d647f613261178147ff69fd9c798e64
U-UL\rossi62u <rossi62u@iutm-inf-f39-10.iutmetz.site.univ-lorraine.fr>
2022-04-29 09:19:30 +0200

    redirect profil to login

--- src/app/login-page/login-page.page.ts
+++ src/app/login-page/login-page.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Validators, FormBuilder } from '@angular/forms';
 import {connect} from '../../services/authServices'
 import { ToastController } from '@ionic/angular';
-import { Router } from '@angular/router';
+import { Router } from '@angular/router'; 
+import { AuthGuardService } from '../services/auth-guard.service';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.page.html',
@@ -12,7 +13,7 @@ export class LoginPagePage implements OnInit {
 
   registerForm;
 
-  constructor(private formBuilder:FormBuilder, public toastController: ToastController, private router : Router) {
+  constructor(private formBuilder:FormBuilder, public toastController: ToastController, private router : Router , public authGuardService : AuthGuardService) {
     this.registerForm = this.formBuilder.group({
       pseudo : ["",[Validators.required]], 
       mdp: ["",[Validators.required]],
@@ -22,15 +23,23 @@ export class LoginPagePage implements OnInit {
   submit(){
     const {pseudo,mdp} = this.registerForm.value;
     if(connect({pseudo,mdp})){this.success() , this.router.navigate(['/'])}
-    else console.log("false")
+    else this.failed();
   }
 
+  async failed(){
+    const toast = await this.toastController.create({
+      message: 'Connexion échoué',
+      duration: 3000
+    });
+    toast.present();
+  }
   async success(){
     const toast = await this.toastController.create({
       message: 'Connecté',
       duration: 3000
     });
     toast.present();
+    this.authGuardService.setAuthenticated(true)
   }
   ngOnInit() {
   }
---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, ActivatedRouteSnapshot } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate{
+  private authenticated= false;
+  getAuthenticated(){
+    return this.authenticated;
+  }
+  setAuthenticated(bool){
+    this.authenticated=bool;
+  }
+  constructor(private router:Router) { }
+  canActivate(route : ActivatedRouteSnapshot) : boolean{
+    console.log(route);
+
+    if(!this.getAuthenticated()){
+      this.router.navigate(['login']);
+      return false;
+    }
+    return true;
+  }
+}
