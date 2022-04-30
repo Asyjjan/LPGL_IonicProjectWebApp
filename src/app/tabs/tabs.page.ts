2e2bb0b9593f6c6158e52700fbd572646d14b74d
U-UL\rossi62u <rossi62u@iutm-inf-f39-10.iutmetz.site.univ-lorraine.fr>
2022-04-30 08:31:32 +0200

    display login tab

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { UserDataService } from '../services/user-data.service';
+import { AuthGuardService } from '../services/auth-guard.service';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
@@ -7,8 +8,10 @@ import { UserDataService } from '../services/user-data.service';
 })
 export class TabsPage {
 
-  constructor(public userDataService : UserDataService) {
+  constructor(public userDataService : UserDataService , public authGuardService : AuthGuardService) {
     console.log(userDataService)
   }
-
+  isLogin(){
+    return this.authGuardService.getAuthenticated();
+  }
 }
