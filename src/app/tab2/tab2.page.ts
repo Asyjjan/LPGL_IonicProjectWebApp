1789bdf696384246c646142bd140d4528cf9948c
U-UL\rossi62u <rossi62u@iutm-inf-f39-11.iutmetz.site.univ-lorraine.fr>
2022-05-07 10:11:43 +0200

    recherche / filtres

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import {findAll} from '../../services/annoncesServices.js'
-import {findById as findUserById} from '../../services/userServices.js'
-import {findById as findAnimalById} from '../../services/animauxServices.js'
-
+import { findAll } from '../../services/annoncesServices.js'
+import { findById as findUserById } from '../../services/userServices.js'
+import { findById as findAnimalById, findAllPath } from '../../services/animauxServices.js'
+import _ from 'lodash'
 import { Annonce } from '../services/annonce';
 import { Router } from '@angular/router';
 @Component({
@@ -11,18 +11,49 @@ import { Router } from '@angular/router';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  public annonces : [];
-  constructor(public router : Router) { 
-    this.annonces= findAll().map(a=>{
-      
+  public annonces: [];
+  private filters = {};
+  public searchQuery = "";
+  constructor(public router: Router) {
+    this.annonces = findAll().map(a => {
+
       const annonce = new Annonce(a)
       annonce.animal = findAnimalById(a.animalId)
       annonce.auteur = findUserById(a.auteurId)
       return annonce
     });
+
   }
 
-  goToDetail(animal){
-    this.router.navigate(['/detail-animal'], {state : { animal}});
+  getFilteredData() {
+    const searched = this.annonces.filter((annonce: Annonce) => annonce.animal.nom.toLowerCase().includes(this.searchQuery.toLowerCase()))
+    if (Object.entries(this.filters).length === 0) return searched
+    const filteredData = searched.filter(({ animal: data }) => {
+      return Object.keys(this.filters).every(filter => {
+        return this.filters[filter] === data[filter]
+      })
+    })
+    return filteredData;
+
+  }
+
+  goToDetail(animal) {
+    this.router.navigate(['/detail-animal'], { state: { animal } });
+  }
+
+  getEspeces(): [] {
+    return findAllPath("espece");
+  }
+  getGenre(): [] {
+    return findAllPath("genre");
+  }
+
+  getLieux(): [] {
+    return findAllPath("lieu")
+  }
+  onFilterChange(pathFilter, event) {
+    const { value } = event.target
+    if (value === "") { delete this.filters[pathFilter]; return }
+    this.filters[pathFilter] = value;
   }
 }
--- src/services/annoncesServices.js
+++ src/services/annoncesServices.js
@@ -1,10 +1,10 @@
 import annonces from '../assets/data/annonces.json'
 export function findAll() {
-    return annonces;
+ return annonces;
 
 }
 export function create(annonce) {
-    annonces.push(annonce)
+ annonces.push(annonce)
 }
 export function destroy(annonceId) {
 
@@ -15,8 +15,6 @@ export function update(annonce) {
 }
 
 export function findById(annonceId) {
-    const result = annonces.filter(a => a.annonceId === annonceId)[0];
-    return result;
+ const result = annonces.filter(a => a.annonceId === annonceId)[0];
+ return result;
 }
-
-
